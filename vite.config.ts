867e3cb04ed2e8de8137d8d63a6e7cd660da0a20
DashaShoo <daryashubabko@gmail.com>
2026-04-12 17:17:08 +0300

    feature: memes & memes-detail pages

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,11 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@styles": path.resolve(__dirname, "./src/styles"),
+      "@hooks": path.resolve(__dirname, "./src/shared/hooks"),
     },
   },
 });
